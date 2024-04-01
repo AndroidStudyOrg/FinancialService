a58281f12929f87e7314537493357c5384f60510
wbham <wbham97@naver.com>
2024-04-01 21:54:55 +0900

    [Refact] - 수정&보완

--- app/src/main/java/org/shop/financialservice/bindingadapter/DataBindingAdapter.kt
+++ app/src/main/java/org/shop/financialservice/bindingadapter/DataBindingAdapter.kt
@@ -4,12 +4,12 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import org.shop.financialservice.R
 
-@BindingAdapter(value=["code_text", "code_index"])
-fun ImageView.setPin(codeText:CharSequence?, index: Int){
-    if (codeText!=null){
-        if (codeText.length>index){
+@BindingAdapter(value = ["code_text", "code_index"])
+fun ImageView.setPin(codeText: CharSequence?, index: Int) {
+    if (codeText != null) {
+        if (codeText.length > index) {
             setImageResource(R.drawable.baseline_circle_black_24)
-        }else{
+        } else {
             setImageResource(R.drawable.baseline_circle_gray_24)
         }
     }
--- app/src/main/java/org/shop/financialservice/widget/ShuffleNumberKeyboard.kt
+++ app/src/main/java/org/shop/financialservice/widget/ShuffleNumberKeyboard.kt
@@ -15,7 +15,7 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : GridLayout(context, attrs, defStyleAttr), View.OnClickListener {
-    /**
+    /** CustomView
      *  Activity 같은 경우 자동으로 bidning instance를 제거해서 지시의 대상이 되는데,
      *  CustomeView에 연결된 binding instance는 자동으로 참조가 끊어지지 않기 때문에 View가 끝날 때 참조를 끊어줘야 한다.
      *  따라서 실질적으로 사용하는 부분은 private으로 하고 직접 가져오는 부분은 private get 메소드만 활용해서 사용하면 된다.
@@ -44,6 +44,7 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
             keyNumberArray.add(i.toString())
         }
 
+        // GridLayout TextView에 숫자 할당
         binding.gridLayout.children.forEach { view ->
             if (view is TextView && view.tag != null) {
                 val randIndex = Random.nextInt(keyNumberArray.size)
