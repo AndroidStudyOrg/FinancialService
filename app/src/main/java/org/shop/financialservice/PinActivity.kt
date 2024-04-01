2cdb131e953d1f828c8483257d5e49c8d9db3d7a
wbham <wbham97@naver.com>
2024-04-01 22:11:50 +0900

    [Feat] - 비밀번호 입력 구체화
    - Pin 입력 시 각종 예외처리 및 정규식 적용

--- app/src/main/java/org/shop/financialservice/PinActivity.kt
+++ app/src/main/java/org/shop/financialservice/PinActivity.kt
@@ -1,6 +1,7 @@
 package org.shop.financialservice
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import org.shop.financialservice.databinding.ActivityPinBinding
@@ -17,6 +18,10 @@ class PinActivity : AppCompatActivity(), ShuffleNumberKeyboard.KeyPadListener {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.shuffleKeyboard.setKeyPadListener(this)
+
+        viewModel.messageLiveData.observe(this) {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onClickNum(num: String) {
